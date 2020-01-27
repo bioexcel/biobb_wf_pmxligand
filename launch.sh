cf805386199746196bf0e71fbe4872bbb67574e5
Pau Andrio <andriopau@gmail.com>
2020-01-27 23:01:26 +0100

    MN4 version

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 source env.sh
 
-enqueue_compss --num_nodes=1 --exec_time=10 --network=ethernet --worker_working_dir=$PWD \
- --qos=debug -d --worker_in_master_cpus=48 --jvm_workers_opts="-Dcompss.worker.removeWD=false" \
- pmxLig.py --config pmxLig.yaml
+enqueue_compss --num_nodes=2 --exec_time=120 --network=ethernet --worker_working_dir=$PWD --master_working_dir=$PWD --base_log_dir=$PWD \
+ --qos=debug -d -g --worker_in_master_cpus=48 --jvm_workers_opts="-Dcompss.worker.removeWD=false" \
+ /home/bsc23/bsc23210/macshare/biobb_wf_pmxligand/pmxLig.py --config /home/bsc23/bsc23210/macshare/biobb_wf_pmxligand/pmxLig.yaml
 
